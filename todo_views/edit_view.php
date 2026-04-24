e9f5374ce5e74c378edb866e7d58ffd81a130be8
m_kimura <m_kimura@craid-inc.com>
2026-04-24 12:20:12 +0900

    レビュー確認

--- todo_views/edit_view.php
+++ todo_views/edit_view.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/../common/common.php';
   <title>ToDo編集画面</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <style>
-  .error{
+          .error{
             border: 1px solid red;
             background-color: #f69d8d;
             padding: 10px;
